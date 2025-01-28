956064db23f9769f5e26d5691d7f47f0a1d80b5f
Praveen-xilli <praveen.r@xilligence.com>
2025-01-28 15:10:04 +0530

    new commit

--- src/test/java/Test_Cases/TC_004.java
+++ src/test/java/Test_Cases/TC_004.java
@@ -2,7 +2,7 @@ package Test_Cases;
 import org.testng.annotations.Test;
 public class TC_004 {
 	@Test()
-	public void exittest() throws IOException
+	public void exittest()
 	{
 		System.out.println("This is fouth test case and the message is ");
 		System.out.println("This is fouth test case and the message is ");
