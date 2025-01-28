9ab49f60dbc14b40fc750a94e7e79818eb4baffb
Praveen-xilli <praveen.r@xilligence.com>
2025-01-28 14:55:12 +0530

    Create TC_004.java

---
+++ TC_004.java
@@ -0,0 +1,12 @@
+package Test_Cases;
+import org.testng.annotations.Test;
+public class TC_004 {
+	@Test()
+	public void exittest() throws IOException
+	{
+		System.out.println("This is fouth test case and the message is ");
+		System.out.println("This is fouth test case and the message is ");
+		System.out.println("This is fouth test case and the message is ");
+		System.out.println("This is fouth test case and the message is ");
+	}
+}
